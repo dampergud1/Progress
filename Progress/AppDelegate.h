4d6f42598b0fb8616f913a62b450e005dd887e3e
Kasper Sølvstrøm Jensen <soelvstroem@hotmail.com>
2013-10-28 12:45:43 +0100

    added xcode ws

---
+++ Progress/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Progress
+//
+//  Created by Kasper Sølvstrøm on 28/10/13.
+//  Copyright (c) 2013 Kasper Sølvstrøm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Progress/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Progress
+//
+//  Created by Kasper Sølvstrøm on 28/10/13.
+//  Copyright (c) 2013 Kasper Sølvstrøm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
